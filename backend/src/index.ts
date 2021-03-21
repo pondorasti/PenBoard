bdb51541e19cc1b9e15b2f1bbb459d459c7b72af
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-20 18:00:42 -0700

    Add NodeJS Backend

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express"
+// import dotenv from "dotenv"
+// import bodyParser from "body-parser"
+// import cors from "cors"
+// import helmet from "helmet"
+// import router from "./routes"
+
+// Setup
+// dotenv.config()
+const app = express()
+const port = process.env.PORT || 8000 // Database Setup
+
+// Middlewares
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(helmet())
+// app.use(cors())
+
+// Routes
+// app.use(router)
+app.get("/", (req: Request, res: Response) => {
+  res.send("API is working")
+})
+
+// Start Server
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`)
+})
+
+export default app
