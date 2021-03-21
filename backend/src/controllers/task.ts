ee35621f49aa06b639b7a85291630fdc8c4c127e
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-20 22:26:55 -0700

    Backend: Add Task Controller and business logic

---
+++ backend/src/controllers/task.ts
@@ -0,0 +1,34 @@
+import TaskModel from "../models/Task"
+import { ITask } from "../interfaces"
+
+export default class TaskService {
+  static async getAll(): Promise<{ [tasks: string]: ITask[] }> {
+    const tasks = await TaskModel.find({})
+    return { tasks }
+  }
+
+  static async get(id: string): Promise<{ [task: string]: ITask }> {
+    const task = await TaskModel.findById(id)
+    return { task }
+  }
+
+  static async create(taskPayload: ITask): Promise<{ [task: string]: ITask }> {
+    const newTask = new TaskModel(taskPayload)
+    const savedTask = await newTask.save()
+
+    return { task: savedTask }
+  }
+
+  static async update(id: string, taskPayload: ITask): Promise<{ [task: string]: ITask }> {
+    await TaskModel.findByIdAndUpdate(id, taskPayload)
+    const task = await TaskModel.findById(id)
+
+    return { task }
+  }
+
+  static async delete(id: string): Promise<{ [task: string]: ITask }> {
+    const task = await TaskModel.findByIdAndDelete(id)
+
+    return { task }
+  }
+}
--- backend/src/routes/task.ts
+++ backend/src/routes/task.ts
@@ -1,33 +1,66 @@
 import { Request, Response, Router } from "express"
+import TaskService from "../controllers/task"
 
 const router = Router()
 
 // Get all Tasks
 router.get("/", async (req: Request, res: Response) => {
-  res.json({ message: "Retrieving all tasks" })
+  try {
+    const tasks = await TaskService.getAll()
+
+    res.json(tasks)
+  } catch (err) {
+    res.json({ error: "of" })
+  }
 })
 
 // Get Task by id
 router.get("/:taskId", async (req: Request, res: Response) => {
-  const { taskId } = req.params
-  res.json({ message: `Retrieving task with id: ${taskId}` })
+  try {
+    const { taskId } = req.params
+    const task = await TaskService.get(taskId)
+
+    res.json(task)
+  } catch (err) {
+    res.json({ error: "of" })
+  }
 })
 
 // Create new Task
 router.post("/", async (req: Request, res: Response) => {
-  res.json({ message: "Creating new task" })
+  try {
+    const taskPayload = req.body
+    const task = await TaskService.create(taskPayload)
+
+    res.json(task)
+  } catch (err) {
+    res.json({ error: "of" })
+  }
 })
 
 // Update Task by id
 router.put("/:taskId", async (req: Request, res: Response) => {
-  const { taskId } = req.params
-  res.json({ message: `Updating task with id: ${taskId}` })
+  try {
+    const { taskId } = req.params
+    const taskPayload = req.body
+    const task = await TaskService.update(taskId, taskPayload)
+
+    res.json(task)
+  } catch (err) {
+    res.json({ error: "of" })
+  }
 })
 
 // Delete Task by id
 router.delete("/:taskId", async (req: Request, res: Response) => {
-  const { taskId } = req.params
-  res.json({ message: `Updating task with id: ${taskId}` })
+  try {
+    const { taskId } = req.params
+    const task = await TaskService.delete(taskId)
+
+    res.json(task)
+  } catch (err) {
+    res.json({ error: "of" })
+  }
 })
 
 export default router
