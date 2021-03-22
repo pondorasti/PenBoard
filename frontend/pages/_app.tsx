41d5b63c8a27afba9814b821ecc9126f7a133676
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-21 20:47:11 -0700

    Frontend: Add redux store and slice

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,18 @@
 import CssBaseline from "@material-ui/core/CssBaseline"
-import { StylesProvider, Box, Typography } from "@material-ui/core/"
+import { StylesProvider } from "@material-ui/core/"
 import { ThemeProvider } from "@material-ui/core/styles"
 import PenTheme from "../components/PenTheme"
+import { Provider } from "react-redux"
+import { store } from "./redux/store"
 
 function MyApp({ Component, pageProps }) {
   return (
     <StylesProvider>
       <ThemeProvider theme={PenTheme}>
         <CssBaseline />
-        <Component {...pageProps} />
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
       </ThemeProvider>
     </StylesProvider>
   )
---
+++ frontend/pages/redux/penBoardSlice.ts
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { RootState } from "./store"
+
+interface IBucket {
+  _id: string
+  name: string
+  index: number
+  tasks: ITask[]
+}
+
+interface ITask {
+  _id: string
+  bucketId: string
+  title: string
+  description?: string
+  storyPoints?: number
+  asignee?: string
+}
+
+// Types
+const name = "penBoard"
+interface PenBoardState {
+  buckets: { [_id: string]: IBucket }
+}
+
+// Initial State
+const initialState: PenBoardState = {
+  buckets: {
+    "3": {
+      _id: "3",
+      name: "ToDo",
+      index: 0,
+      tasks: [
+        { _id: "1", title: "First task", bucketId: "3" },
+        { _id: "2", title: "Second task", bucketId: "3" },
+      ],
+    },
+    "4": {
+      _id: "4",
+      name: "Done",
+      index: 1,
+      tasks: [
+        { _id: "3", title: "Third task", bucketId: "4" },
+        { _id: "4", title: "Forth task", bucketId: "4" },
+      ],
+    },
+  },
+}
+
+// Slice
+const penBoardSlice = createSlice({ name, initialState, reducers: {}, extraReducers: {} })
+
+// Selectors
+export const selectBuckets = (state: RootState) => state.penBoard.buckets
+
+// Slice Reducer
+export default penBoardSlice.reducer
