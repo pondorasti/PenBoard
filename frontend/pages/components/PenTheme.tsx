598ed3149366d7568af271b4dd3145f2c07a4ea1
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-22 00:06:44 -0700

    Frontend: Improve theme

--- frontend/pages/components/PenTheme.tsx
+++ frontend/pages/components/PenTheme.tsx
@@ -4,12 +4,26 @@ import { Shadows } from "@material-ui/core/styles/shadows"
 const PenTheme = createMuiTheme({
   palette: {
     mode: "dark",
+    primary: {
+      main: "rgb(95, 108, 203)",
+    },
     background: {
       default: "rgb(28, 29, 31)",
       paper: "rgb(39, 40, 43)",
     },
   },
   shadows: Array(25).fill("none") as Shadows,
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-root": {
+            borderRadius: 12,
+          },
+        },
+      },
+    },
+  },
 })
 
 export default PenTheme
