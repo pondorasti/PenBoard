a51dbadc4fd85c77193b208ba9ec99175002b113
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-11-26 21:25:53 -0800

    add: default baseUrl for api

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL
+const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "https://penboard-backend.dev.alexandru.wtf"
 
 export const bucketRef = `${baseUrl}/bucket`
 export const taskRef = `${baseUrl}/task`
